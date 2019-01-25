bcc58084c1b5b8ff45536da947cc46f4a7d9ae41
wedwer666 <mitriucmaria@gmail.com>
2019-01-25 18:19:11 +0200

    Update RunnableExample.java

--- src/main/java/RunnableExample.java
+++ src/main/java/RunnableExample.java
@@ -1,13 +1,14 @@
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class RunnableExample implements Runnable {
+    public static AtomicInteger suma =  new AtomicInteger();
 
-    public void run() {
+     public void run() {
         Integer generatedNumber = new Random().nextInt((10)) + 1;
-          int testRandom = RunnableMainClass.genNum.getAndIncrement();
-//         RunnableMainClass.genNum.addAndGet(generatedNumber);
-        System.out.println("Thread name " + Thread.currentThread().getName() + ", generated number " + testRandom);
-
+        RunnableMainClass.genNum.getAndAdd(generatedNumber);
+        suma.getAndAdd(generatedNumber);
+        System.out.println("Thread name " + Thread.currentThread().getName() + ", generated number " + generatedNumber);
 
     }
 }
